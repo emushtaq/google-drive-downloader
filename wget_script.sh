303a39b89e16c220419d81341ad49ea501cce911
Eshan Mushtaq <eshanmushtaq@gmail.com>
2018-05-23 16:00:58 +0200

    Create wget_script.sh
    added wget option

---
+++ wget_script.sh
@@ -0,0 +1,2 @@
+# WORKS with large file requiring confirmation. Replace FILENAME and FILEID with relevant values
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=FILEID" -O FILENAME && rm -rf /tmp/cookies.txt
